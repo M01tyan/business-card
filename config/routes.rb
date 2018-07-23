a3c094b2707060bfdd9512fc94f5b1f46e1bff07
EC2 Default User <ec2-user@ip-172-31-31-55.us-east-2.compute.internal>
2018-07-23 13:18:47 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'messages#index'
+  root 'visit_companies#index'
   resources :visit_companies
   resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
